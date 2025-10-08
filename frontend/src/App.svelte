6926e044b4a0d2ad91cb7699e6e282dc431a426a
yjcho <yjcho2013@gmail.com>
2025-10-08 19:29:41 +0900

    파이보 프로젝트 최초 커밋

---
+++ frontend/src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Router from 'svelte-spa-router'
+    import Home from "./routes/Home.svelte"
+    import Detail from "./routes/Detail.svelte"
+    import QuestionCreate from "./routes/QuestionCreate.svelte"
+    import Navigation from "./components/Navigation.svelte"
+    import UserCreate from "./routes/UserCreate.svelte"
+    import UserLogin from "./routes/UserLogin.svelte"
+
+    const routes = {
+        '/': Home,
+        '/detail/:question_id': Detail,
+        '/question-create': QuestionCreate,
+        '/user-create' : UserCreate,
+        '/user-login': UserLogin,
+    }
+    </script>
+
+    <Navigation />
+    <Router {routes}/>
